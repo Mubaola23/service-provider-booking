ced76d9d982669b3163c66df08b46ddaa09e6056
mubaola23 <mubarakshuaib3@gmail.com>
2025-11-23 09:59:38 +0100

    feat: added method to select date,time and duration

--- lib/screens/providers/booking_screen_provider.dart
+++ lib/screens/providers/booking_screen_provider.dart
@@ -1,1 +1,59 @@
+import 'package:hooks_riverpod/legacy.dart';
 
+class BookingState {
+  final DateTime? selectedDate;
+  final String? selectedTime;
+  final int? selectedDuration;
+  final double? totalPrice;
+  final bool isConfirmEnabled;
+
+  BookingState({
+    this.selectedDate,
+    this.selectedTime,
+    this.selectedDuration,
+    this.totalPrice,
+    this.isConfirmEnabled = false,
+  });
+
+  BookingState copyWith({
+    DateTime? selectedDate,
+    String? selectedTime,
+    int? selectedDuration,
+    double? totalPrice,
+    bool? isConfirmEnabled,
+  }) {
+    return BookingState(
+      selectedDate: selectedDate ?? this.selectedDate,
+      selectedTime: selectedTime ?? this.selectedTime,
+      selectedDuration: selectedDuration ?? this.selectedDuration,
+      totalPrice: totalPrice ?? this.totalPrice,
+      isConfirmEnabled: isConfirmEnabled ?? this.isConfirmEnabled,
+    );
+  }
+}
+
+class BookingScreenNotifier extends StateNotifier<BookingState> {
+  final double hourlyRate;
+
+  BookingScreenNotifier(this.hourlyRate) : super(BookingState());
+
+  void selectDate(DateTime date) {
+    state = state.copyWith(selectedDate: date);
+  }
+
+  void selectTime(String time) {
+    state = state.copyWith(selectedTime: time);
+  }
+
+  void selectDuration(int duration) {
+    state = state.copyWith(selectedDuration: duration);
+  }
+}
+
+final bookingProvider =
+    StateNotifierProvider.family<BookingScreenNotifier, BookingState, double>((
+      ref,
+      hourlyRate,
+    ) {
+      return BookingScreenNotifier(hourlyRate);
+    });
