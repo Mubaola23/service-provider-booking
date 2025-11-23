0cdf8ec29a3cf898558d12022616b7343c6a064a
mubaola23 <mubarakshuaib3@gmail.com>
2025-11-23 09:34:56 +0100

    updates the main.dart to display provider list

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:service_provider_assessment/screens/provider_list_screen.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         primaryColor: const Color(0xFF2E7D32),
         scaffoldBackgroundColor: const Color(0xFFF7F8FC),
       ),
-      home: Container(),
+      home: ProviderListScreen(),
     );
   }
 }
